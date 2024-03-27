415a4eb8186a4979edc5ad8c1e58c3e52fed9e8d
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-03-27 01:28:39 +0100

    Add documentation comments to `new` neuron method

--- src/neuron.rs
+++ src/neuron.rs
@@ -2,8 +2,9 @@ use crate::tensor::Tensor;
 
 /// Represents a neuron with its parameters.
 ///
-/// Weights are initialized during the first forward pass because at this stage,
-/// we don't know the needed number of weights. It depend on the number of input.
+/// Weights are an option because since the number of weights depends on the number of inputs, we
+/// can't define the weights at neuron's creation. Weights are initialized during the first forward
+/// pass.
 #[derive(Debug, PartialEq)]
 pub struct Neuron<'a> {
     pub weights: Option<Tensor<'a>>,
@@ -11,6 +12,18 @@ pub struct Neuron<'a> {
 }
 
 impl<'a> Neuron<'a> {
+    /// Creates a new `Neuron` instance from a bias.
+    ///
+    /// Since the number of weights depends on the number of inputs, we can't define the weights at
+    /// neuron's creation. Weights are initialized during the first forward pass.
+    ///
+    /// # Argument
+    ///
+    /// * `bias` - A `f32` letting developer choose its way to initialize it.
+    ///
+    /// # Returns
+    ///
+    /// A `Neuron` instance.
     pub fn new(bias: f32) -> Neuron<'a> {
         Neuron {
             weights: None,
