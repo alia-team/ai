1b67793b3ce36e34cb5cf83c6293b55c787bd9c2
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-03-27 22:30:57 +0100

    Add `forward` method to neuron

--- src/neuron.rs
+++ src/neuron.rs
@@ -11,6 +11,11 @@ pub struct Neuron<'a> {
     pub bias: f32,
 }
 
+#[derive(Debug, PartialEq)]
+pub enum NeuronError {
+    WeightsNotInitialized,
+}
+
 impl<'a> Neuron<'a> {
     /// Creates a new `Neuron` instance from a bias.
     ///
@@ -30,5 +35,32 @@ impl<'a> Neuron<'a> {
             weights: None,
             bias,
         }
+    }
+
+    /// Performs a forward pass on the neuron with given inputs.
+    ///
+    /// This method calculates the weighted sum of the inputs and the neuron's
+    /// bias.
+    /// It requires that the weights of the neuron have been initialized
+    /// beforehand.
+    /// If the weights are not initialized, it returns a
+    /// `NeuronError::WeightsNotInitialized` error.
+    ///
+    /// # Arguments
+    ///
+    /// * `inputs` - A reference to a `Tensor` representing the inputs to the
+    /// neuron.
+    ///
+    /// # Returns
+    ///
+    /// An `Ok(f32)` containing the result of the weighted sum plus the bias if
+    /// the weights are initialized.
+    /// Otherwise, returns a `NeuronError::WeightsNotInitialized` error.
+    pub fn forward(&self, inputs: &Tensor<'a>) -> Result<f32, NeuronError> {
+        if self.weights.is_none() {
+            return Err(NeuronError::WeightsNotInitialized);
+        }
+
+        Ok(self.weights.as_ref().unwrap().dot(inputs).unwrap() + self.bias)
     }
 }
