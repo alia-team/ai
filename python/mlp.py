cf81b86bf6b814226a4d7cafcc750ebf3765d9b6
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-05-16 23:07:36 +0200

    Create MLP class

--- python/mlp.py
+++ python/mlp.py
@@ -29,45 +29,51 @@ lib.mlp_train.restype = None
 lib.mlp_free.argtypes = [ctypes.c_void_p]
 lib.mlp_free.restype = None
 
-# Create an instance of MLP
-npl = (2, 3, 1)
-npl_arr = (ctypes.c_size_t * len(npl))(*npl)
-mlp = lib.mlp_new(npl_arr, len(npl))
+class MLP:
+    def __init__(self, npl):
+        npl_arr = (ctypes.c_size_t * len(npl))(*npl)
+        self.mlp = lib.mlp_new(npl_arr, len(npl))
+        self.output_size = npl[-1]
 
-# Prepare the training dataset and labels
-training_dataset = [
-    [0.0, 0.0],
-    [0.0, 1.0],
-    [1.0, 0.0]
-]
-labels = [
-    [-1.0],
-    [1.0],
-    [1.0]
-]
+    def train(self, training_dataset, labels, alpha, nb_iter, is_classification):
+        samples_count = len(training_dataset)
+        sample_inputs_len = len(training_dataset[0])
+        
+        TrainingDatasetType = ctypes.POINTER(ctypes.c_double) * samples_count
+        training_dataset_ctypes = TrainingDatasetType(*[np.ctypeslib.as_ctypes(np.array(sample, dtype=np.float64)) for sample in training_dataset])
+        
+        LabelsType = ctypes.POINTER(ctypes.c_double) * samples_count
+        labels_ctypes = LabelsType(*[np.ctypeslib.as_ctypes(np.array(label, dtype=np.float64)) for label in labels])
+        
+        lib.mlp_train(self.mlp, training_dataset_ctypes, labels_ctypes, samples_count, sample_inputs_len, alpha, nb_iter, is_classification)
+    
+    def predict(self, sample_inputs, is_classification):
+        sample_inputs_arr = np.ctypeslib.as_ctypes(np.array(sample_inputs, dtype=np.float64))
+        output_ptr = lib.mlp_predict(self.mlp, sample_inputs_arr, len(sample_inputs), is_classification)
+        output = [output_ptr[i] for i in range(self.output_size)]
+        return output
+    
+    def __del__(self):
+        lib.mlp_free(self.mlp)
 
-samples_count = len(training_dataset)
-sample_inputs_len = len(training_dataset[0])
-
-# Convert the training dataset and labels to ctypes-compatible structures
-TrainingDatasetType = ctypes.POINTER(ctypes.c_double) * samples_count
-training_dataset_ctypes = TrainingDatasetType(*[np.ctypeslib.as_ctypes(np.array(sample, dtype=np.float64)) for sample in training_dataset])
-
-LabelsType = ctypes.POINTER(ctypes.c_double) * samples_count
-labels_ctypes = LabelsType(*[np.ctypeslib.as_ctypes(np.array(label, dtype=np.float64)) for label in labels])
-
-# Train the MLP
-alpha = 0.1
-nb_iter = 1000000
-is_classification = True
-lib.mlp_train(mlp, training_dataset_ctypes, labels_ctypes, samples_count, sample_inputs_len, alpha, nb_iter, is_classification)
-
-# Example prediction
-sample_inputs = [0.0, 1.0]
-sample_inputs_arr = np.ctypeslib.as_ctypes(np.array(sample_inputs, dtype=np.float64))
-output_ptr = lib.mlp_predict(mlp, sample_inputs_arr, len(sample_inputs), False)
-output = [output_ptr[i] for i in range(npl[-1])]
-print(output)
-
-# Free the MLP instance
-lib.mlp_free(mlp)
+# Example usage:
+if __name__ == "__main__":
+    npl = (2, 1)
+    mlp = MLP(npl)
+    
+    training_dataset = [
+        [0.0, 0.0],
+        [0.0, 1.0],
+        [1.0, 0.0]
+    ]
+    labels = [
+        [-1.0],
+        [1.0],
+        [1.0]
+    ]
+    
+    mlp.train(training_dataset, labels, 0.1, 1000000, True)
+    
+    for k in range(len(training_dataset)):
+        output = mlp.predict(training_dataset[k], True)
+        print(output)
