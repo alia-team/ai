29eb1f196d7055009ed9439f0c6fb8ff3e963332
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-05-17 01:31:29 +0200

    Add tests cases for mlp

--- python/test-cases.py
+++ python/test-cases.py
@@ -20,47 +20,73 @@ labels = np.array([
 
 mlp.train(training_dataset, labels, 0.1, 1000000, True)
 
-for k in range(len(training_dataset)):
-    output = mlp.predict(training_dataset[k], True)
-    print(output)
-
-
-# Linear Simple
-# MLP (2, 1): OK
-plt.scatter(training_dataset[0, 0], training_dataset[0, 1], color='blue')
-plt.scatter(training_dataset[1:3,0], training_dataset[1:3,1], color='red')
-plt.show()
-plt.clf()
+new_point = [1, 1]
+result = mlp.predict(new_point, True)
+print('Linear simple:', result)
 
 
 # Linear Multiple :
 # MLP (2, 1): OK
+npl = (2, 1)
+mlp = MLP(npl)
+
 training_dataset = np.concatenate([np.random.random((50,2)) * 0.9 + np.array([1, 1]), np.random.random((50,2)) * 0.9 + np.array([2, 2])])
 labels = np.concatenate([np.ones((50, 1)), np.ones((50, 1)) * -1.0])
 
-plt.scatter(training_dataset[0:50, 0], training_dataset[0:50, 1], color='blue')
-plt.scatter(training_dataset[50:100,0], training_dataset[50:100,1], color='red')
-plt.show()
-plt.clf()
+mlp.train(training_dataset, labels, 0.1, 10000, True)
 
+new_point = [2.75, 2.75]
+result = mlp.predict(new_point, True)
+print('Linear Multiple:', result)
 
 # XOR
 # MLP (2, 2, 1): 
+npl = (2, 4, 1)
+mlp = MLP(npl)
 training_dataset = np.array([[1, 0], [0, 1], [0, 0], [1, 1]])
-labels = np.array([1, 1, -1, -1])
+labels = np.array([[1], [1], [-1], [-1]])
 
-plt.scatter(training_dataset[0:2, 0], training_dataset[0:2, 1], color='blue')
-plt.scatter(training_dataset[2:4,0], training_dataset[2:4,1], color='red')
-plt.show()
-plt.clf()
+mlp.train(training_dataset, labels, 0.1, 1000000, True)
+
+new_point = [1, 1]
+result = mlp.predict(new_point, True)
+print('XOR:', result)
 
 
 # Cross
-# MLP (2, 4, 1): 
+# MLP (2, 4, 1)
+npl = (2, 5, 1)
+mlp = MLP(npl)
+
+# Initialize the training dataset and labels
 training_dataset = np.random.random((500, 2)) * 2.0 - 1.0
 labels = np.array([1 if abs(p[0]) <= 0.3 or abs(p[1]) <= 0.3 else -1 for p in training_dataset])
+labels = labels.reshape(-1, 1)  # Ensure labels are in the correct shape
 
-plt.scatter(np.array(list(map(lambda elt : elt[1], filter(lambda c: Y[c[0]] == 1, enumerate(training_dataset)))))[:,0], np.array(list(map(lambda elt : elt[1], filter(lambda c: Y[c[0]] == 1, enumerate(training_dataset)))))[:,1], color='blue')
-plt.scatter(np.array(list(map(lambda elt : elt[1], filter(lambda c: Y[c[0]] == -1, enumerate(training_dataset)))))[:,0], np.array(list(map(lambda elt : elt[1], filter(lambda c: Y[c[0]] == -1, enumerate(training_dataset)))))[:,1], color='red')
-plt.show()
-plt.clf()
+mlp.train(training_dataset, labels, 0.1, 1000000, True)
+
+new_point = [-1, -1]
+result = mlp.predict(new_point, True)
+print('Cross:', result)
+
+
+# Multi linear 3 classes
+# MLP (2, 3)
+npl = (2, 3)
+mlp = MLP(npl)
+
+# Initialize the training dataset and labels
+training_dataset = np.random.random((500, 2)) * 2.0 - 1.0
+labels = np.array([[1, 0, 0] if -p[0] - p[1] - 0.5 > 0 and p[1] < 0 and p[0] - p[1] - 0.5 < 0 else
+              [0, 1, 0] if -p[0] - p[1] - 0.5 < 0 and p[1] > 0 and p[0] - p[1] - 0.5 < 0 else
+              [0, 0, 1] if -p[0] - p[1] - 0.5 < 0 and p[1] < 0 and p[0] - p[1] - 0.5 > 0 else
+              [0, 0, 0]for p in training_dataset])
+
+training_dataset = training_dataset[[not np.all(arr == [0, 0, 0]) for arr in labels]]
+labels = labels[[not np.all(arr == [0, 0, 0]) for arr in labels]]
+
+mlp.train(training_dataset, labels, 0.1, 1000, True)
+
+new_point = [1, 1]
+result = mlp.predict(new_point, True)
+print('Multi linear 3 classes:', result)
