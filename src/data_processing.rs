d9656b74a36c624555b786fcdbb366ce4b8de7d9
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-05-23 16:12:03 +0200

    Convert `u8` values to `f64` in `image_to_vector`

--- src/data_processing.rs
+++ src/data_processing.rs
@@ -5,7 +5,7 @@ pub enum ImageError {
     ImageNotProcceded,
 }
 
-pub fn image_to_vector(image_path: &str) -> Result<Vec<u8>, ImageError> {
+pub fn image_to_vector(image_path: &str) -> Result<Vec<f64>, ImageError> {
     let img = image::open(image_path).expect("Failed to open image");
 
     let (width, height) = img.dimensions();
@@ -16,7 +16,7 @@ pub fn image_to_vector(image_path: &str) -> Result<Vec<u8>, ImageError> {
         for x in 0..width {
             let pixel = img.get_pixel(x, y);
             let channels = pixel.to_rgb().0;
-            pixel_values.extend_from_slice(&channels);
+            pixel_values.extend(channels.iter().map(|&v| v as f64));
         }
     }
     Ok(pixel_values)
