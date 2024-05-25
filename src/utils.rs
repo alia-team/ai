d01ea68616b5d74e0b20666f2f5d73e6b63ea865
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-05-25 23:54:06 +0200

    Add `init_outputs`

--- src/utils.rs
+++ src/utils.rs
@@ -1,6 +1,30 @@
 extern crate rand;
 use rand::Rng;
 
+pub fn init_outputs(neurons_per_layer: Vec<usize>) -> Vec<Vec<f64>> {
+    let mut outputs: Vec<Vec<f64>> = vec![];
+
+    // Skip output layer
+    for layer in 0..(neurons_per_layer.len() - 1) {
+        outputs.push(vec![]);
+
+        // +1 for bias
+        for neuron in 0..(neurons_per_layer[layer] + 1) {
+            if neuron == 0 {
+                // Bias
+                outputs[layer].push(1.0)
+            } else {
+                // Weight between -1.0 and 1.0
+                outputs[layer].push(0.0)
+            }
+        }
+    }
+
+    outputs.push(vec![0.0; *neurons_per_layer.last().unwrap()]);
+
+    outputs
+}
+
 pub fn init_weights(neurons_per_layer: Vec<usize>) -> Vec<Vec<Vec<f64>>> {
     let mut weights: Vec<Vec<Vec<f64>>> = vec![];
 
