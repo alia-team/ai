08b7f131d38407d9b79dc0645d82327431cd5ac1
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-05-25 23:57:07 +0200

    Add `init_outputs` (#61)

--- tests/utils.rs
+++ tests/utils.rs
@@ -1,6 +1,36 @@
 use ai::utils;
 
 #[test]
+fn init_outputs() {
+    let neurons_per_layer: Vec<usize> = vec![2, 3, 1];
+    let outputs: Vec<Vec<f64>> = utils::init_outputs(neurons_per_layer.clone());
+
+    // Check number of layers
+    assert_eq!(outputs.len(), 3);
+
+    for layer in 0..outputs.len() {
+        // Check output layer separately since it doens't have a bias
+        if layer == outputs.len() - 1 {
+            assert_eq!(outputs[layer].len(), neurons_per_layer[layer]);
+            for output in 0..outputs[layer].len() {
+                assert_eq!(outputs[layer][output], 0.0);
+            }
+            continue;
+        }
+
+        // Check other layers
+        assert_eq!(outputs[layer].len(), neurons_per_layer[layer] + 1);
+        for output in 0..outputs[layer].len() {
+            if output == 0 {
+                assert_eq!(outputs[layer][output], 1.0); // Bias
+                continue;
+            }
+            assert_eq!(outputs[layer][output], 0.0)
+        }
+    }
+}
+
+#[test]
 fn init_weights() {
     let neurons_per_layer: Vec<usize> = vec![2, 3, 1];
     let weights: Vec<Vec<Vec<f64>>> = utils::init_weights(neurons_per_layer.clone());
