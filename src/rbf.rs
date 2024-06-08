e63ebd9ef55d9d61983110cfe1fdceac7f1fc63f
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-06-08 19:26:04 +0200

    Add interop for RBF (#77)
    * Add new RBF external function
    
    * Add fit RBF external function
    
    * Add predict RBF external function
    
    * Add free RBF external function
    
    * Add safety doc for unsafe functions
    
    * Add ctypes bindings in python
    
    * Create RBF class in python

--- src/rbf.rs
+++ src/rbf.rs
@@ -3,6 +3,7 @@ use crate::activation::sign;
 use crate::utils;
 use nalgebra::DMatrix;
 use rand::Rng;
+use std::{ptr, slice};
 
 #[derive(Debug, PartialEq)]
 pub struct Centroid {
@@ -186,4 +187,115 @@ impl RBF {
         // Assign weights to the last layer
         self.weights[2] = weights;
     }
+}
+
+/// # Safety
+///
+/// This function assumes that the pointers `neurons_per_layer` and
+/// `training_dataset` are valid and that they point to arrays of `layers_count`
+/// and `training_dataset_nrows` elements respectively.
+/// Each element of the `training_dataset` should be a pointer to an array of
+/// `training_dataset_ncols` elements.
+/// The caller must ensure that these conditions are met to avoid undefined
+/// behavior.
+#[no_mangle]
+pub unsafe extern "C" fn new_rbf(
+    neurons_per_layer: *const usize,
+    layers_count: usize,
+    is_classification: bool,
+    training_dataset: *const *const f64,
+    training_dataset_nrows: usize,
+    training_dataset_ncols: usize,
+) -> *mut RBF {
+    // Convert neurons_per_layer to Vec<usize>
+    let npl_slice: &[usize] = unsafe { slice::from_raw_parts(neurons_per_layer, layers_count) };
+    let npl_vec: Vec<usize> = npl_slice.to_vec();
+
+    // Convert training_dataset to Vec<Vec<f64>>
+    let mut training_dataset_vec: Vec<Vec<f64>> = Vec::with_capacity(training_dataset_nrows);
+    for i in 0..training_dataset_nrows {
+        let row_slice: &[f64] =
+            unsafe { slice::from_raw_parts(*training_dataset.add(i), training_dataset_ncols) };
+        training_dataset_vec.push(row_slice.to_vec());
+    }
+
+    let rbf: RBF = RBF::new(npl_vec, is_classification, training_dataset_vec);
+    let boxed_rbf: Box<RBF> = Box::new(rbf);
+
+    Box::leak(boxed_rbf)
+}
+
+/// # Safety
+///
+/// This function assumes that the pointers `training_dataset` and `labels` are
+/// valid and point to arrays of `training_dataset_nrows` and `labels_len`
+/// elements respectively.
+/// Each element of the `training_dataset` should be a pointer to an array of
+/// `training_dataset_ncols` elements.
+/// The caller must ensure that these conditions are met to avoid undefined
+/// behavior.
+#[no_mangle]
+pub unsafe extern "C" fn fit_rbf(
+    rbf_ptr: *mut RBF,
+    training_dataset: *const *const f64,
+    training_dataset_nrows: usize,
+    training_dataset_ncols: usize,
+    labels: *const *const f64,
+    labels_nrows: usize,
+    labels_ncols: usize,
+    max_iterations: usize,
+) {
+    // Convert training_dataset to Vec<Vec<f64>>
+    let mut training_dataset_vec: Vec<Vec<f64>> = Vec::with_capacity(training_dataset_nrows);
+    for i in 0..training_dataset_nrows {
+        let row_slice: &[f64] =
+            unsafe { slice::from_raw_parts(*training_dataset.add(i), training_dataset_ncols) };
+        training_dataset_vec.push(row_slice.to_vec());
+    }
+
+    // Convert labels to Vec<Vec<f64>>
+    let mut labels_vec: Vec<Vec<f64>> = Vec::with_capacity(labels_nrows);
+    for i in 0..labels_nrows {
+        let row_slice: &[f64] = unsafe { slice::from_raw_parts(*labels.add(i), labels_ncols) };
+        labels_vec.push(row_slice.to_vec());
+    }
+
+    if let Some(rbf) = unsafe { rbf_ptr.as_mut() } {
+        rbf.fit(training_dataset_vec, labels_vec, max_iterations);
+    }
+}
+
+/// # Safety
+///
+/// This function assumes that the pointers `rbf_ptr` and `input` are valid and
+/// points to a valid `RBF` instance and to an array of `input_len` elements
+/// respectively.
+/// The caller must ensure that these conditions are met to avoid undefined
+/// behavior.
+#[no_mangle]
+pub unsafe extern "C" fn predict_rbf(
+    rbf_ptr: *mut RBF,
+    input: *const f64,
+    input_len: usize,
+) -> *const f64 {
+    // Convert input to Vec<f64>
+    let input_slice: &[f64] = unsafe { slice::from_raw_parts(input, input_len) };
+    let input_vec: Vec<f64> = input_slice.to_vec();
+
+    if let Some(rbf) = unsafe { rbf_ptr.as_mut() } {
+        rbf.predict(input_vec).as_ptr()
+    } else {
+        ptr::null()
+    }
+}
+
+/// # Safety
+///
+/// This function assumes that the pointer `rbf_ptr` is valid and points to a
+/// valid `RBF` instance.
+/// The caller must ensure that this condition is met to avoid undefined
+/// behavior.
+#[no_mangle]
+pub unsafe extern "C" fn free_rbf(rbf_ptr: *mut RBF) {
+    let _ = unsafe { Box::from_raw(rbf_ptr) };
 }
