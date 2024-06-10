16bbab67496d95454b3e357a1f1df40316ef58a2
Romain <romain.nerot@outlook.fr>
2024-06-10 21:54:52 +0200

    Remove useless test

--- tests/data_processing.rs
+++ tests/data_processing.rs
@@ -22,18 +22,3 @@ fn image_to_vector() {
         assert_eq!(pixel_values_slice, expected_values.as_slice());
     };
 }
-
-#[test]
-fn get_all_images_in_folder() {
-    let folder_path = CString::new("tests\\test_images").unwrap();
-    let images_ptr = unsafe {
-        data_processing::get_all_images_in_folder(folder_path.as_ptr())
-    };
-
-    let images_len = unsafe {
-        let images_slice = slice::from_raw_parts(images_ptr, 541);
-        images_slice.len()
-    };
-
-    assert_eq!(images_len, 541);
-}
