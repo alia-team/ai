f3cba1290385c364e154925b571c70d123e038e6
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-06-15 12:26:29 -0400

    Add UT for tanh activation function

--- tests/activation.rs
+++ tests/activation.rs
@@ -27,3 +27,10 @@ fn logistic() {
     assert_eq!(activation::logistic(0.0), 0.5);
     assert_eq!(activation::logistic(1.0), 0.7310585786300049);
 }
+
+#[test]
+fn tanh() {
+    assert_eq!(activation::tanh(-1.0), -0.7615941559557649);
+    assert_eq!(activation::tanh(0.0), 0.0);
+    assert_eq!(activation::tanh(1.0), 0.7615941559557649);
+}
