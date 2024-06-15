062632bfcbb6107ecb6b32349c9ce626b8c6a129
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-06-15 12:22:09 -0400

    Add UT for logistic activation function

--- tests/activation.rs
+++ tests/activation.rs
@@ -6,3 +6,10 @@ fn sign() {
     assert_eq!(activation::sign(0.0), 1.0);
     assert_eq!(activation::sign(1.0), 1.0);
 }
+
+#[test]
+fn logistic() {
+    assert_eq!(activation::logistic(-1.0), 0.2689414213699951);
+    assert_eq!(activation::logistic(0.0), 0.5);
+    assert_eq!(activation::logistic(1.0), 0.7310585786300049);
+}
