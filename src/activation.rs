c101329e39a9778b40bdb53b342a63ce0a2ce815
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-06-15 11:55:51 -0400

    Add tanh & sigmoid activations

--- src/activation.rs
+++ src/activation.rs
@@ -1,7 +1,15 @@
-pub enum Activation {
-    SIGN,
-    HEAVISIDE,
-    IDENTITY,
+pub type Activation = fn(f64) -> f64;
+
+pub fn string_to_activation(string: &str) -> Activation {
+    match string {
+        "sign" => sign,
+        "heaviside" => heaviside,
+        "identity" => identity,
+        "logistic" => logistic,
+        "sigmoid" => sigmoid,
+        "tanh" => tanh,
+        _ => panic!("Not a supported activation function"),
+    }
 }
 
 pub fn sign(x: f64) -> f64 {
@@ -22,4 +30,14 @@ pub fn heaviside(x: f64) -> f64 {
 
 pub fn identity(x: f64) -> f64 {
     x
+}
+
+pub fn logistic(x: f64) -> f64 {
+1.0 / (1.0 + (-x).exp())
+}
+
+pub fn sigmoid = logistic;
+
+pub fn tanh(x: f64) -> f64 {
+    x.tanh()
 }
