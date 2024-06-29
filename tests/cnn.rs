1a5a85ba318035637c976f8cfef491d4adc65ede
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-06-29 11:51:25 -0400

    Add convolutional function

--- tests/cnn.rs
+++ tests/cnn.rs
@@ -1,4 +1,4 @@
-use ai::cnn::{self, Padding};
+use ai::cnn::{self, Activation, Padding};
 
 #[test]
 fn max_pool_2d() {
@@ -41,4 +41,40 @@ fn max_pool_2d() {
         cnn::max_pool_2d(&input, pool_size, stride, padding),
         expected_output
     );
+}
+
+#[test]
+fn conv_2d() {
+    let input: Vec<Vec<f64>> = vec![
+        vec![7.0, 2.0, 3.0, 3.0, 8.0],
+        vec![4.0, 5.0, 3.0, 8.0, 4.0],
+        vec![3.0, 3.0, 2.0, 8.0, 4.0],
+        vec![2.0, 8.0, 7.0, 2.0, 7.0],
+        vec![5.0, 4.0, 4.0, 5.0, 4.0],
+    ];
+    let strides = (1, 1);
+    let kernel = Some(vec![
+        vec![1.0, 0.0, -1.0],
+        vec![1.0, 0.0, -1.0],
+        vec![1.0, 0.0, -1.0],
+    ]);
+    let kernel_size = None;
+    let filters = 1;
+    let padding = Padding::Valid;
+    let activation = Activation::Identity;
+    let output = cnn::conv_2d(
+        &input,
+        strides,
+        kernel,
+        kernel_size,
+        filters,
+        padding,
+        activation,
+    );
+    let expected_output: Vec<Vec<f64>> = vec![
+        vec![6.0, -9.0, -8.0],
+        vec![-3.0, -2.0, -3.0],
+        vec![-3.0, 0.0, -2.0],
+    ];
+    assert_eq!(output, expected_output);
 }
