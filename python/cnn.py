035ecc87a79e9243b49cd848415eee9bec136bb5
Opixelum <48217573+opixelum@users.noreply.github.com>
2024-07-23 19:37:06 +0200

    Add interop for CNN

---
+++ python/cnn.py
@@ -0,0 +1,221 @@
+from ctypes import cast, string_at, c_char_p, c_double, c_longlong, c_size_t, c_void_p, CDLL, POINTER
+import numpy as np
+import platform
+from util import c_array_to_list, list_to_c_array
+
+system = platform.system()
+if system == 'Linux':
+    lib_filename = 'libai.so'
+elif system == 'Darwin':  # macOS
+    lib_filename = 'libai.dylib'
+elif system == 'Windows':
+    lib_filename = 'ai.dll'
+else:
+    raise RuntimeError(f"Unsupported operating system: {system}")
+
+lib = CDLL(f"./target/release/{lib_filename}")
+
+lib.new_cnn.argtypes = [
+    c_char_p,   # dataset_path
+    c_double,   # train_ratio
+    c_longlong, # image_per_class
+    c_size_t,   # batch_size
+    c_size_t,   # epochs
+    c_char_p,   # optimizer
+    c_double,   # learning_rate
+    c_double,   # optimizer_param2
+    c_double    # optimizer_param3
+]
+lib.new_cnn.restype = c_void_p
+
+lib.set_input_shape.argtypes = [
+    c_void_p, # cnn_ptr
+    POINTER(c_size_t) # input_shape
+]
+lib.set_input_shape.restype = None
+
+lib.add_conv2d_layer.argtypes = [
+    c_void_p, # cnn_ptr
+    c_size_t, # nfilters
+    c_size_t  # kernel_size
+]
+lib.set_input_shape.restype = None
+
+lib.add_maxpool2d_layer.argtypes = [
+    c_void_p, # cnn_ptr
+    c_size_t  # kernel_size
+]
+lib.add_maxpool2d_layer.restype = None
+
+lib.add_dense_layer.argtypes = [
+    c_void_p, # cnn_ptr
+    c_size_t, # output_size
+    c_char_p, # activation
+    c_double, # dropout
+    c_char_p  # weights_init
+]
+lib.add_dense_layer.restype = None
+
+lib.fit_cnn.argtypes = [
+    c_void_p # cnn_ptr
+]
+lib.fit_cnn.restype = None
+
+lib.predict_cnn.argtypes = [
+    c_void_p, # cnn_ptr
+    c_char_p # image_path
+]
+lib.predict_cnn.restype = POINTER(c_double)
+
+lib.save_cnn.argtypes = [
+    c_void_p, # cnn_ptr
+    c_char_p, # path
+    c_char_p  # model_name
+]
+lib.save_cnn.restype = c_char_p
+
+lib.load_cnn.argtypes = [
+    c_char_p # model_path
+]
+lib.load_cnn.restype = c_void_p
+
+lib.free_cnn.argtypes = [
+    c_void_p # cnn_ptr
+]
+lib.free_cnn.restype = None
+
+class CNN:
+    def __init__(self) -> None:
+        self.output_size: int = 0
+        self.model = None
+
+    def setup(
+        self,
+        dataset_path: str,
+        train_ratio: float,
+        image_per_class: int,
+        batch_size: int,
+        epochs: int,
+        optimizer: str,
+        learning_rate: float,
+        optimizer_param2: float,
+        optimizer_param3: float
+    ) -> None:
+        dataset_path_c_str: bytes = dataset_path.encode("utf-8")
+        optimizer_c_str: bytes = optimizer.encode("utf-8")
+
+        self.model = lib.new_cnn(
+            dataset_path_c_str,
+            train_ratio,
+            image_per_class,
+            batch_size,
+            epochs,
+            optimizer_c_str,
+            learning_rate,
+            optimizer_param2,
+            optimizer_param3 
+        )
+
+    def set_input_shape(self, input_shape: list[int]):
+        input_shape_c = list_to_c_array(input_shape, c_size_t)
+        lib.set_input_shape(self.model, input_shape_c)
+
+    def add_conv2d_layer(self, nfilters: int, kernel_size: int):
+        lib.add_conv2d_layer(self.model, nfilters, kernel_size)
+
+    def add_maxpool2d_layer(self, kernel_size: int):
+        lib.add_maxpool2d_layer(self.model, kernel_size)
+
+    def add_dense_layer(self, output_size: int, activation: str, dropout: float, weights_init: str):
+        self.output_size = output_size
+        activation_c_str: bytes = activation.encode("utf-8")
+        weights_init_c_str: bytes = weights_init.encode("utf-8")
+        lib.add_dense_layer(self.model, output_size, activation_c_str, dropout, weights_init_c_str)
+
+    def fit(self) -> None:
+        lib.fit_cnn(self.model)
+
+    def predict(self, image_path: str) -> list[float]:
+        output = lib.predict_cnn(
+            self.model,
+            image_path.encode("utf-8")
+        )
+        return c_array_to_list(output, self.output_size)
+
+    def save(self, path: str, model_name: str) -> str:
+        path_c_str: bytes = path.encode("utf-8")
+        model_name_c_str: bytes = model_name.encode("utf-8")
+        full_path_c = lib.save_cnn(self.model, path_c_str, model_name_c_str)
+        full_path = string_at(full_path_c).decode("utf-8")
+        return full_path
+
+    def free(self) -> None:
+        lib.free_cnn(self.model)
+
+
+def load_cnn(model_path: str) -> CNN:
+    cnn = CNN()
+    cnn.model = lib.load_cnn(model_path.encode("utf-8"))
+    cnn.output_size = 3 # To refactor so we get it dynamically
+    return cnn
+
+
+print("Initializing CNN...")
+cnn = CNN()
+dataset_path: str = "./dataset/"
+train_ratio: float = 0.8
+image_per_class: int = 100
+batch_size: int = 10
+epochs: int = 10
+optimizer: str = "adam"
+learning_rate: float = 0.001
+beta1: float = 0.9
+beta2: float = 0.9
+cnn.setup(
+    dataset_path,
+    train_ratio,
+    image_per_class,
+    batch_size,
+    epochs,
+    optimizer,
+    learning_rate,
+    beta1,
+    beta2
+)
+cnn.set_input_shape([100, 100, 3]);
+cnn.add_conv2d_layer(8, 3);
+cnn.add_maxpool2d_layer(2);
+cnn.add_dense_layer(128, "relu", 0.25, "he");
+cnn.add_dense_layer(64, "relu", 0.25, "he");
+cnn.add_dense_layer(3, "softmax", 0., "xavier");
+
+print("Fitting...")
+cnn.fit()
+
+print("Saving model...")
+full_path: str = cnn.save("./models/", "python_240")
+print("Freeing CNN...")
+cnn.free()
+print("Freed.")
+
+print("Loading model...")
+loaded_cnn: CNN = load_cnn(full_path)
+
+print('Predicting... It should predict "phidippus".')
+image_path: str = "./dataset/phidippus/835255150-388.png"
+output: list[float] = loaded_cnn.predict(image_path)
+predicted: str = ""
+match output.index(max(output)):
+    case 0:
+        predicted = "avicularia"
+    case 1:
+        predicted = "phidippus"
+    case 2:
+        predicted = "tegenaria"
+    case _:
+        predicted = "error during prediction"
+print(f"Predicted: {predicted}")
+
+print("Freeing loaded CNN...")
+loaded_cnn.free()
+print("Freed.")
--- src/cnn_interop.rs
+++ src/cnn_interop.rs
@@ -1,11 +1,15 @@
+use crate::activation::{str_to_activation, Activation};
 use crate::data::{load_image_dataset, Dataset3D};
 use crate::model::{Hyperparameters, CNN};
 use crate::optimizer::{str_to_optmizer, Optimizer};
 use crate::util::c_str_to_rust_str;
+use crate::weights_init::{str_to_weights_init, WeightsInit};
+use core::slice;
 use libc::{c_char, c_double, c_longlong, size_t};
-use ndarray::Array3;
+use ndarray::Array1;
 use std::ffi::CString;
 
+#[no_mangle]
 pub unsafe extern "C" fn new_cnn(
     dataset_path: *const c_char,
     train_ratio: c_double,
@@ -45,32 +49,65 @@ pub unsafe extern "C" fn new_cnn(
     Box::leak(Box::new(CNN::new(dataset, hyperparameters)))
 }
 
-pub unsafe extern "C" fn fit_cnn(cnn_ptr: *mut CNN) {
-    unsafe { cnn_ptr.as_mut().expect("Null CNN pointer.").fit() }
+#[no_mangle]
+pub unsafe extern "C" fn set_input_shape(cnn_ptr: *mut CNN, input_shape: *const size_t) {
+    let cnn: &mut CNN = unsafe { cnn_ptr.as_mut().expect("Null CNN pointer.") };
+    let input_shape: Vec<usize> = unsafe { slice::from_raw_parts(input_shape, 3).to_vec() };
+    cnn.set_input_shape(input_shape);
 }
 
+#[no_mangle]
+pub unsafe extern "C" fn add_conv2d_layer(
+    cnn_ptr: *mut CNN,
+    nfilters: size_t,
+    kernel_size: size_t,
+) {
+    let cnn: &mut CNN = unsafe { cnn_ptr.as_mut().expect("Null CNN pointer.") };
+    cnn.add_conv2d_layer(nfilters, kernel_size);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn add_maxpool2d_layer(cnn_ptr: *mut CNN, kernel_size: size_t) {
+    let cnn: &mut CNN = unsafe { cnn_ptr.as_mut().expect("Null CNN pointer.") };
+    cnn.add_maxpool2d_layer(kernel_size);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn add_dense_layer(
+    cnn_ptr: *mut CNN,
+    output_size: size_t,
+    activation: *const c_char,
+    dropout: c_double,
+    weights_init: *const c_char,
+) {
+    let cnn: &mut CNN = unsafe { cnn_ptr.as_mut().expect("Null CNN pointer.") };
+    let dropout: Option<f64> = if dropout == 0. { None } else { Some(dropout) };
+    let activation: Box<dyn Activation> = str_to_activation(c_str_to_rust_str(activation));
+    let weights_init: WeightsInit = str_to_weights_init(c_str_to_rust_str(weights_init));
+    cnn.add_dense_layer(output_size, activation, dropout, weights_init);
+}
+
+#[no_mangle]
+pub unsafe extern "C" fn fit_cnn(cnn_ptr: *mut CNN) {
+    let cnn: &mut CNN = unsafe { cnn_ptr.as_mut().expect("Null CNN pointer.") };
+    cnn.fit();
+}
+
+#[no_mangle]
 pub unsafe extern "C" fn predict_cnn(
     cnn_ptr: *mut CNN,
-    input: *const *const *const c_double,
-    nrows: size_t,
-    ncols: size_t,
-    nchannels: size_t,
+    image_path: *const c_char,
 ) -> *const c_double {
-    // Convert input to Array3
-    let input: Array3<f64> = unsafe {
-        Array3::from_shape_fn((nrows, ncols, nchannels), |(i, j, k)| {
-            *(*(*input.add(i)).add(j)).add(k)
-        })
-    };
-
-    let cnn = unsafe { cnn_ptr.as_mut().expect("Null CNN pointer.") };
-    let output = cnn.predict(input);
-    let output_ptr = output.as_ptr();
+    let cnn: &mut CNN = unsafe { cnn_ptr.as_mut().expect("Null CNN pointer.") };
+    let image_path: &str = c_str_to_rust_str(image_path);
+    let output: Array1<f64> = cnn.predict(image_path);
+    let output_ptr: *const f64 = output.as_ptr();
     std::mem::forget(output);
 
     output_ptr
 }
 
+#[no_mangle]
 pub unsafe extern "C" fn save_cnn(
     cnn_ptr: *mut CNN,
     path: *const c_char,
@@ -81,14 +118,15 @@ pub unsafe extern "C" fn save_cnn(
     let model_name: &str = c_str_to_rust_str(model_name);
     let full_path: CString =
         CString::new(cnn.save(path, model_name)).expect("Failed to convert Rust str to C str.");
-
-    full_path.as_ptr()
+    full_path.into_raw()
 }
 
+#[no_mangle]
 pub unsafe extern "C" fn load_cnn(model_path: *const c_char) -> *mut CNN {
     Box::leak(Box::new(CNN::load(c_str_to_rust_str(model_path))))
 }
 
+#[no_mangle]
 pub unsafe extern "C" fn free_cnn(cnn_ptr: *mut CNN) {
     let _ = unsafe { Box::from_raw(cnn_ptr) };
 }
