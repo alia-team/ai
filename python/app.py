01461804e90645f0be23a64310f248b12b6a1c82
jogoml <johanribl@gmail.com>
2024-07-23 10:08:41 +0200

    Add app

---
+++ python/app.py
@@ -0,0 +1,73 @@
+import streamlit as st
+import os
+from PIL import Image
+from mlp import *
+#from cnn import *
+from naive_rbf import *
+from rbf import *
+import data_processing as dp
+
+def convert_to_palette_mode_file(image):
+    image = Image.open(image)
+    image = image.convert('P', palette=Image.ADAPTIVE, colors=256)
+    return image
+
+def resize_image(image, size):
+    image = Image.open(image)
+    image = image.resize(size)
+    return image
+
+st.title('ALIA')
+
+st.write('Bienvenu sur ALIA, l\'application qui vous permet d\'en apprendre plus sur les araignées et de savoir à laquelle vous avez a faire.')
+
+option = st.selectbox(
+    'Quel modèle souhaitez-vous utiliser ?',
+    ('MLP', 'CNN', 'Naive RBF', 'RBF')
+)
+
+uploaded_file = st.file_uploader("Choisissez une image...", type="png")
+
+if uploaded_file is not None:
+    image = Image.open(uploaded_file)
+
+    st.image(image, caption='Uploaded Image.', use_column_width=True)
+    st.write("")
+    st.write("Classifying...")
+    image = resize_image(uploaded_file, (100, 100))
+    image = convert_to_palette_mode_file(uploaded_file)
+
+    image.save('tmp_img/image.png')
+    img_vector = dp.image_to_vector('tmp_img/image.png')
+
+    # if option == 'MLP':
+    #     model = MLP()
+    #     model.load_model('models/mlp')
+    #     prediction = model.predict(img_vector)
+    # elif option == 'CNN':
+    #     model = CNN()
+    #     model.load_model('models/cnn')
+    #     prediction = model.predict(img_vector)
+    # elif option == 'Naive RBF':
+    #     model = NaiveRBF()
+    #     model.load_model('models/naive_rbf')
+    #     prediction = model.predict(img_vector)
+    # elif option == 'RBF':
+    #     model = RBF()
+    #     model.load_model('models/rbf')
+    #     prediction = model.predict(img_vector)
+
+    
+
+    os.remove('tmp_img/image.png')
+
+    # clean page
+    st.empty()
+
+    st.write('Done')
+
+    # st.write(f'Prediction: {prediction}')
+
+
+
+
