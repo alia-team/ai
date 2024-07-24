b0d5cf944ab191520fe725f1498ead75d9f24c3c
johan <94543237+jogoml@users.noreply.github.com>
2024-07-24 17:22:39 +0200

    Feature/144 app add connection (#146)
    * Add mlp
    
    * Fix dataset path
    
    * Add mlp app
    
    * modify inputs mlp
    
    ---------
    
    Co-authored-by: Opixelum <48217573+opixelum@users.noreply.github.com>

--- python/app.py
+++ python/app.py
@@ -33,17 +33,25 @@ if uploaded_file is not None:
 
     if option == 'MLP':
         st.write("Loading model...")
-        model = load_mlp('../models/mlp.json')
+        model = load_mlp('../models/mlp_best.json')
 
         st.write("Predicting...")
-        img_vector = image_to_vector(standardized_img)
-        prediction = model.predict(img_vector, True)
+        img = Image.open('tmp_img/image.png')
+        inputs = image_to_vector(img)
+        image_mean = inputs.mean()
+        image_std = inputs.std()
+        inputs = [(i - image_mean) / image_std for i in inputs]
+        prediction = model.predict(inputs, True)
+
 
     elif option == 'CNN':
         st.write("Loading model...")
         model = load_cnn('../models/cnn_240.json')
 
         st.write("Predicting...")
+        if not os.path.exists('tmp_img'):
+            os.makedirs('tmp_img')
+
         prediction = model.predict('tmp_img/image.png')
 
     # elif option == 'Naive RBF':
@@ -62,7 +70,10 @@ if uploaded_file is not None:
     #     st.write("Predicting...")
     #     prediction = model.predict(img_vector)
 
+
     os.remove('tmp_img/image.png')
+    print(prediction)
+
     max_index = prediction.index(max(prediction))
 
     if max_index == 0:
@@ -74,8 +85,5 @@ if uploaded_file is not None:
     else:
         prediction = 'Error during prediction.'
 
+
     st.write(f'Prediction: {prediction}')
-
-
-
-
